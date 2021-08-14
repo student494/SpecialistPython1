41dca839f0a1066922ba9138c7b6db38e06619c7
student494 <88583674+student494@users.noreply.github.com>
2021-08-14 16:21:33 +0300

    Update 05_task_list.py

--- Module3/practice/list_and_tuple/05_task_list.py
+++ Module3/practice/list_and_tuple/05_task_list.py
@@ -6,7 +6,23 @@
 fruits = ["яблоко", "банан", "киви", "арбуз"]
 
 # TODO: your code here
+# Дан список фруктов.
+# Напишите программу, выводящую фрукты в виде нумерованного списка,
+# выровненного по правой стороне - слева короткие слова дополняем символами пробела
 
+# Исходные данные:
+fruits = ["яблоко", "банан", "киви", "арбуз"]
+n = 0
+for element in fruits:
+    i = len(element)
+    if n < i:
+        n = i
+j = len(str(len(fruits)))
+max_long_str = j + 2 + n
+i = 1
+for element in fruits:
+    print(str(i) + ". " + (" "*(max_long_str - len(element) - 2 - len(str(i)))) + str(element))
+    i += 1
 # Пример вывода:
 # 1. яблоко
 # 2.  банан
