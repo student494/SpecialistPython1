3d8862864c9f80aaa87023b4981828f59e49e854
student494 <88583674+student494@users.noreply.github.com>
2021-08-26 20:26:18 +0300

    Update 02_hw_func.py

--- Module4/home_work/02_hw_func.py
+++ Module4/home_work/02_hw_func.py
@@ -5,9 +5,18 @@
 # При решении задачи необходимо использовать функцию расстояния между двумя точками.
 
 def distance(x1, y1, x2, y2):
-    # TODO: тело, которое вы реализовали на практической работе
-    pass
+    return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 
 
-# TODO: your code here
-print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
+xa = 1
+ya = 1
+xb = 3
+yb = 5
+xc = 10
+yc = 4
+AB = distance(xa, ya, xb, yb)
+AC = distance(xa, ya, xc, yc)
+BC = distance(xb, yb, xc, yc)
+dict_line = dict(AB="отрезок AB", AC="отрезок AC", BC="отрезок BC")
+min_line = min(dict_line)
+print(f"Самый короткий {dict_line[min_line]}")  # Выводим название отрезка, например “АС”.
