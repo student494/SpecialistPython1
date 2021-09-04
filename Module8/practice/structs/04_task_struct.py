fc17ea5165f57b5d2158a21b7d17ac01e9d781ef
student494 <88583674+student494@users.noreply.github.com>
2021-09-04 16:17:24 +0300

    Update 04_task_struct.py

--- Module8/practice/structs/04_task_struct.py
+++ Module8/practice/structs/04_task_struct.py
@@ -2,3 +2,6 @@
 dictionary_1 = {'a': 300, 'b': 400}
 dictionary_2 = {'c': 500, 'd': 600}
 # Объедините их в один
+for keys, values in dictionary_2.items():
+    dictionary_1[keys] = values
+print(dictionary_1)
